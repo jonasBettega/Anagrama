7c44df6e7387e7b4d540f9ec1fba3981747b84ba
jonasbettega <jonas.bettega@gmail.com>
2016-01-24 18:48:36 -0200

    segundo do dia 24 janeiro

---
+++ app/models/word.rb
@@ -0,0 +1,14 @@
+class Word < ActiveRecord::Base
+
+
+  validate :verifica_anagrama
+  validates_presence_of :word1, :word2 
+  validates_uniqueness_of :word1, :word2
+  
+  private
+  def verifica_anagrama
+    errors.add(:word, "Não é um anagrama") unless word1.downcase.chars.sort == word2.downcase.chars.sort && word1     != word2
+  end
+
+
+end
